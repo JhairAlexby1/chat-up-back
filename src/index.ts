3038764e5ecd7c76cb917221bf46336d998fabd4
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-05-29 10:29:57 -0600

    agregado al cors: app.use(cors({                       origin: 'http://localhost:3000', // reemplaza con la URL de tu front-end                       credentials: true,                       methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],                       allowedHeaders: ['Content-Type', 'Authorization']                   })); para dejar que acepte cookies de dominios diferentes (pendiente supervicion )

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors(
-    {
-        methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    }
-));
+app.use(cors({
+    origin: 'http://localhost:3000', // reemplaza con la URL de tu front-end
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
