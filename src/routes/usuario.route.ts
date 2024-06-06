6a7802962831199ec37d692f67616ad73e4b46b3
SwordGenius <swordgenius222@gmail.com>
2024-06-06 15:09:24 -0600

    incoming changes 13

--- src/routes/usuario.route.ts
+++ src/routes/usuario.route.ts
@@ -1,13 +1,14 @@
 import express from "express";
 
 import UsuarioController from "../controllers/usuario.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.get("/", UsuarioController.index);
 router.post("/", UsuarioController.create);
 router.post("/login", UsuarioController.login);
-router.get("/conected", UsuarioController.indexConected);
-router.post("/logout", UsuarioController.logout);
+router.get("/conected", verifyToken, UsuarioController.indexConected);
+router.post("/logout", verifyToken, UsuarioController.logout);
 
 export default router;
